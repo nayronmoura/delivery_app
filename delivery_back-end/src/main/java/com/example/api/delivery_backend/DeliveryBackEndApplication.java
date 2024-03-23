a745770e5369ae60593589ac19e434c1dcb7b819
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-03-23 14:44:05 -0300

    chore: wip

---
+++ delivery_back-end/src/main/java/com/example/api/delivery_backend/DeliveryBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.api.delivery_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryBackEndApplication.class, args);
+	}
+
+}
---
+++ delivery_back-end/src/test/java/com/example/api/delivery_backend/DeliveryBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api.delivery_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
